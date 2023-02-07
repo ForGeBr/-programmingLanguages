2b442f7a0f27e6d7c0502ce7802ea3ccb11aea32
Aleksandr <77ginn77@bk.ru>
2023-02-07 11:54:25 +0300

    домашнее задание к 3ему семинару

---
+++ Домашняя работа/Задача21/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 21
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+
+Console.WriteLine("Введите координаты x, y, z для точки А (Через Enter):");
+int x = int.Parse(Console.ReadLine()!), y = int.Parse(Console.ReadLine()!), z = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите координаты x1, y1, z1 для точки В (Через Enter):");
+int x1 = int.Parse(Console.ReadLine()!), y1 = int.Parse(Console.ReadLine()!), z1 = int.Parse(Console.ReadLine()!);
+
+double res = Math.Round(Math.Sqrt(Math.Pow(x-x1, 2) + Math.Pow(y-y1, 2) + Math.Pow(z-z1, 2)), 2);
+
+Console.WriteLine($"Расстояние между тремя точками: {res}");
---
+++ Домашняя работа/Задача23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+Console.Write("Введте число: ");
+int a = int.Parse(Console.ReadLine()!);
+int count = 1;
+
+while (count <= a)
+{
+    Console.Write(count * count * count);
+    if (count != a) Console.Write(", ");
+    count++;
+}
