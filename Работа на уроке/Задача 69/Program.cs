b7d0531a54a88fa1956fa337ac8feda6414248ea
Aleksandr <77ginn77@bk.ru>
2023-02-25 11:46:54 +0300

    Урок 9

---
+++ Работа на уроке/Задача 69/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии
+
+int RaicingToPower(int a, int b)
+{
+    if (b == 0) return 1;
+    else 
+    {
+        return a * RaicingToPower(a, b - 1);
+    }
+}
+
+Console.WriteLine("введите число : ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите cтепень : ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine(RaicingToPower(n, m));
